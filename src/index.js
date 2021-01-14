dc9343977b631dcec70a3044b6c0a0c8856f0f93
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2021-01-15 02:48:49 +0300

    Shift conditionals to switch statement..

--- src/index.js
+++ src/index.js
@@ -1,24 +1,31 @@
-import './style.css';
-import { restaurantTitle, createMenuTabs } from './dom';
-import { createMenuPage } from './menu';
-import { createContactPage } from './contact';
+import "./style.css";
+import { restaurantTitle, createMenuTabs } from "./dom";
+import { createMenuPage } from "./menu";
+import { createContactPage } from "./contact";
 
-const contentDiv = document.getElementById('content');
+const contentDiv = document.getElementById("content");
 contentDiv.appendChild(restaurantTitle());
 contentDiv.appendChild(createMenuTabs());
 
-const menuTab = document.getElementById('menu');
-menuTab.addEventListener('click', () => { 
-    alert("Menu");
-    document.getElementById('contact').style.display = "none";
-    // document.getElementById('restaurant-menu').style.display = "flex";
-    contentDiv.appendChild(createMenuPage());
+const openMenu = (id) => {
+  switch (id) {
+    case "menu":
+      alert("Menu");
+      break;
+
+    case "contact":
+      alert("Contact");
+    default:
+      break;
+  }
+};
+
+const menuTab = document.getElementById("menu");
+menuTab.addEventListener("click", () => {
+    openMenu(menuTab.id);
 });
 
-const contactTab = document.getElementById('contact');
-contactTab.addEventListener('click', () => {
-    alert("Contact");
-    document.getElementById('restaurant-menu').style.display = "none";
-    // document.getElementById('contact').style.display = "flex";
-    contentDiv.appendChild((createContactPage()));
+const contactTab = document.getElementById("contact");
+contactTab.addEventListener("click", () => {
+    openMenu(contactTab.id);
 });
