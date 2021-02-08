94d57aec64e28cc5030ec4824ef8e3189fd023c5
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2021-02-08 20:54:20 +0300

    Remove unused imports.

--- src/dom.js
+++ src/dom.js
@@ -24,28 +24,6 @@ const createMenuTabs = () => {
   return tabMenu;
 };
 
-
-const itemList = (type) => {
-  const itemDB = document.createElement('ul');
-  const appetizeTitle = document.createElement('h3');
-  appetizeTitle.className = 'category-title';
-  appetizeTitle.innerText = `${type}`;
-  const item1 = document.createElement('li');
-  item1.innerText = `${type}` + ' One';
-  const item2 = document.createElement('li');
-  item2.innerText = `${type}` + ' Two';
-  const item3 = document.createElement('li');
-  item3.innerText = `${type}` + ' Three';
-  const item4 = document.createElement('li');
-  item4.innerText = `${type}` + ' Four';
-  itemDB.appendChild(appetizeTitle);
-  itemDB.appendChild(item1);
-  itemDB.appendChild(item2);
-  itemDB.appendChild(item3);
-  itemDB.appendChild(item4);
-  return itemDB;
-};
-
-export { restaurantTitle, itemList, createMenuTabs };
+export { restaurantTitle, createMenuTabs };
 
 /* eslint no-useless-concat: off */
